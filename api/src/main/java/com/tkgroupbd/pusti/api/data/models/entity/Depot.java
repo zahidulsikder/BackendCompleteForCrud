cd8fef201d9bd9a8fb052dad53c23b8fbf919fc2
zahidulsikder <mdzahidulsikder@gmail.com>
2023-07-11 17:26:24 +0600

    Lombok used

--- api/src/main/java/com/tkgroupbd/pusti/api/data/models/entity/Depot.java
+++ api/src/main/java/com/tkgroupbd/pusti/api/data/models/entity/Depot.java
@@ -3,10 +3,17 @@ package com.tkgroupbd.pusti.api.data.models.entity;
 import com.tkgroupbd.pusti.api.data.models.common.Sample;
 import com.tkgroupbd.pusti.api.data.models.enums.Department;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Depot {
 
     @Id
@@ -21,78 +28,7 @@ public class Depot {
     private Department department;
 
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Sample{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", salary=" + salary +
-                ", department=" + department +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Depot depot = (Depot) o;
-        return Double.compare(depot.salary, salary) == 0 && Objects.equals(id, depot.id) && Objects.equals(name, depot.name) && Objects.equals(phoneNumber, depot.phoneNumber) && Objects.equals(email, depot.email) && department == depot.department;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, phoneNumber, email, salary, department);
-    }
 
 
 }
--- api/src/main/java/com/tkgroupbd/pusti/api/data/payload/request/DepotRequest.java
+++ api/src/main/java/com/tkgroupbd/pusti/api/data/payload/request/DepotRequest.java
@@ -3,11 +3,12 @@ package com.tkgroupbd.pusti.api.data.payload.request;
 import com.tkgroupbd.pusti.api.data.models.enums.Department;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
+@Data
 public class DepotRequest {
 
 
@@ -32,45 +33,7 @@ public class DepotRequest {
    private Department department;
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
 
 
 }
